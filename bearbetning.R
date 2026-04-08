7fe3768f0b75414e1290de2ee8af1c14bd69accd
RFSISUUppland <appar.uppland@rfsisu.se>
2026-04-08 14:43:54 +0200

    Uppdatering

---
+++ bearbetning.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(magrittr)
+library(knitr)
+library(readxl)
+library(openxlsx)
+library(janitor)
+
+
+
+data <- read_excel("C:\\Users\\WILIDF17\\OneDrive - Sveriges Riksidrottsförbund\\Dokument\\GitHub\\RF-SISU Uppland\\SocialaMedier\\data.xlsx")
+
+data <- clean_names(data)
+data$publish_time <- as.Date(data$publish_time, "%y/%m/%d")
+
+data <- data %>% 
+  mutate(
+    source = case_when(
+      str_detect(post_type, "IG") ~ "Instagram",
+      TRUE                        ~ "Facebook"
+    ),
+    description = str_trunc(description, width = 75),
+    published_by = case_when(
+      account_name != "RF-SISU Uppland" ~ "Annan",
+      TRUE                              ~ account_name
+    )
+  ) %>% 
+  select(post_id, permalink, publish_time, source, account_name, published_by, description, views, likes, shares, comments, saves, reach, follows, total_clicks, other_clicks, duration_sec)
+
+write.xlsx(data, "data.xlsx")
---
+++ kör_bearbetning.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d "%~dp0"
+
+"C:\Users\WILIDF17\AppData\Local\Programs\R\R-4.5.2\bin\Rscript.exe" bearbetning.R
+
+pause
