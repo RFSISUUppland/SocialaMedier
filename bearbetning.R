a41829ccfa8848ca7e0e78ac7bf8e128c32f95ae
RFSISUUppland <appar.uppland@rfsisu.se>
2026-04-09 13:28:08 +0200

    Uppdatering

--- bearbetning.R
+++ bearbetning.R
@@ -9,7 +9,8 @@ library(openxlsx)
 library(janitor)
 
 
-rådata <- read_excel("C:\\Users\\WILIDF17\\OneDrive - Sveriges Riksidrottsförbund\\Dokument\\GitHub\\RF-SISU Uppland\\SocialaMedier\\rådata.xlsx")
+rådata <- read_xlsx("C:\\Users\\WILIDF17\\OneDrive - Sveriges Riksidrottsförbund\\Dokument\\GitHub\\RF-SISU Uppland\\SocialaMedier\\rådata.xlsx",
+                    sheet = "rådata")
 
 data <- clean_names(rådata)
 data$publish_time <- as.Date(data$publish_time, "%y/%m/%d")
@@ -24,9 +25,32 @@ data <- data %>%
     published_by = case_when(
       account_name != "RF-SISU Uppland" ~ "Annan",
       TRUE                              ~ account_name
+    ),
+    post_type = case_when(
+      post_type %in% c("IG image", "Photos") ~ "Bild",
+      post_type %in% c("IG reel", "Videos") ~ "Video",
+      post_type %in% c("IG carousel") ~ "Karusell",
+      post_type %in% c("Links") ~ "Länkar (FB)",
+      post_type %in% c("Text") ~ "Text (FB)",
+      TRUE ~ post_type
     )
   ) %>% 
-  select(post_id, permalink, publish_time, source, account_name, published_by, description, views, likes, shares, comments, saves, reach, follows, total_clicks, other_clicks, duration_sec)
+  select(post_id, permalink, publish_time, source, account_name, published_by, description, post_type, views, likes, shares, comments, saves, reach, follows, total_clicks, other_clicks, duration_sec)
 
-write.xlsx(data, "data.xlsx")
+
+
+wb <- loadWorkbook("data.xlsx")
+
+ 
+removeWorksheet(wb, "content")
+
+addWorksheet(wb, "content")
+
+
+writeData(wb, sheet = "content", data)
+
+
+saveWorkbook(wb, "data.xlsx", overwrite = TRUE)
+
+
 
