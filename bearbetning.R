da9c79ec8178d946b7bf2a1f1ab715b094cced87
RFSISUUppland <appar.uppland@rfsisu.se>
2026-04-13 09:54:05 +0200

    Uppdatering

--- bearbetning.R
+++ bearbetning.R
@@ -3,10 +3,12 @@ library(dplyr)
 library(tidyr)
 library(stringr)
 library(magrittr)
+library(lubridate)
 library(knitr)
 library(readxl)
 library(openxlsx)
 library(janitor)
+
 
 
 rådata <- read_xlsx("C:\\Users\\WILIDF17\\OneDrive - Sveriges Riksidrottsförbund\\Dokument\\GitHub\\RF-SISU Uppland\\SocialaMedier\\rådata.xlsx",
@@ -16,12 +18,18 @@ data <- clean_names(rådata)
 data$publish_time <- as.Date(data$publish_time, "%y/%m/%d")
 
 data <- data %>% 
+  filter(!( (is.na(views)   | views   == "") &
+             (is.na(likes)   | likes   == "") &
+             (is.na(shares)  | shares  == "") &
+             (is.na(comments)| comments== "") &
+             (is.na(reach)   | reach   == "") )) %>%
+  
   mutate(
     source = case_when(
       str_detect(post_type, "IG") ~ "Instagram",
       TRUE                        ~ "Facebook"
     ),
-    description = str_trunc(description, width = 75),
+    description = str_trunc(description, width = 100),
     published_by = case_when(
       account_name != "RF-SISU Uppland" ~ "Annan",
       TRUE                              ~ account_name
@@ -35,12 +43,12 @@ data <- data %>%
       TRUE ~ post_type
     )
   ) %>% 
-  select(post_id, permalink, publish_time, source, account_name, published_by, description, post_type, views, likes, shares, comments, saves, reach, follows, total_clicks, other_clicks, duration_sec)
+   
+  select(post_id, permalink, publish_time, source, account_name, published_by, description, post_type, category, views, likes, shares, comments, saves, reach, follows, total_clicks, other_clicks, duration_sec)
 
 
 
 wb <- loadWorkbook("data.xlsx")
-
 
 removeWorksheet(wb, "content")
 
@@ -49,7 +57,5 @@ addWorksheet(wb, "content")
 writeData(wb, sheet = "content", data)
 
 saveWorkbook(wb, "data.xlsx", overwrite = TRUE)
-
-
 
 
